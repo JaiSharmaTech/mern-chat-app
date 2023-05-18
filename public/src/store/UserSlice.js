829a7d9c23fa611733421d891ae5287d65bab95e
KartikeyKakaria <kartikeykakaria2@gmail.com>
2023-05-18 21:12:35 +0530

    added thunks for fetching contacts

--- public/src/store/UserSlice.js
+++ public/src/store/UserSlice.js
@@ -1,13 +1,32 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { allUsersRoute } from "../utils/ApiRoutes";
+import axios from "axios"
 const initialState = {
     currentUser:JSON.parse(localStorage.getItem("chat-app-user")) || null,
     currentChat:{},
     chatUsers:[],
-    loadedChatUsers:false,
+    loadedChatUsers:"loading",
+    error:null,
 }
+
+export const fetchChatUsers = createAsyncThunk('user/fetchChatUsers',async(userId)=>{
+    const {data} = await axios.get(`${allUsersRoute}/${userId}`);
+    return data;
+})
+
 const usersSlice = createSlice({
     name:"user",
-    initialState
+    initialState,
+    extraReducers:(builder)=>{
+        builder.addCase(fetchChatUsers.fulfilled,(state, action)=>{
+            state.chatUsers = action.payload
+            state.loadedChatUsers = "loaded"
+        })
+        builder.addCase(fetchChatUsers.rejected,(state, action)=>{
+            state.loadedChatUsers = "error"
+            state.error = action.error
+        })
+    }
 })
 
 export default usersSlice.reducer
--- public/src/store/messagesSlice.js
+++ public/src/store/messagesSlice.js
@@ -1,11 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-    messages:[],
-    messagesLoaded:false,
+    messages: [],
+    messagesLoaded: false,
 }
 const messageSlice = createSlice({
-    name:"messages",
+    name: "messages",
     initialState,
+    reducers: {
+        addMessage(state, action) {
+            state.push({
+                fromSelf: true,
+                message: action.payload,
+            })
+        }
+    }
 })
 
 export default messageSlice.reducer
